cb43cd4b2f2c1cac481275a958b5cd0acba24ad6
NINGauw <haonam.vt01@gmail.com>
2024-03-24 22:43:27 +0700

    Add movement system

--- Stellar Blast/Assets/Rocket.cs
+++ Stellar Blast/Assets/Rocket.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rigidBody;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         ProcessInput();
@@ -20,13 +20,13 @@ public class Rocket : MonoBehaviour
     private void ProcessInput()
     {
         if(Input.GetKey(KeyCode.Space)){
-    
+            rigidBody.AddRelativeForce(Vector3.up);
         }
         if(Input.GetKey(KeyCode.A)){
-            
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D)){
-            
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
