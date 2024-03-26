6c4090e0e79ca015c27300cc224348c7a876aa0b
NINGauw <haonam.vt01@gmail.com>
2024-03-26 20:51:58 +0700

    Add sound & Stage 1 design

--- Stellar Blast/Assets/Rocket.cs
+++ Stellar Blast/Assets/Rocket.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     private Rigidbody rigidBody;
-
+    [SerializeField]private AudioSource thrustSound;
+    [SerializeField]private float rotationspeed;
+    [SerializeField]private float thrustSpeed;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -14,19 +16,46 @@ public class Rocket : MonoBehaviour
 
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
+    }
+    void OnCollisionEnter(Collision collision){
+        switch(collision.gameObject.tag){
+            case "Friendly":
+            print("friendly");
+            break;
+            default:
+            print("dead");
+            break;
+        }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationspeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationspeed * Time.deltaTime);
+        }
+        rigidBody.freezeRotation = false;
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space)){
-            rigidBody.AddRelativeForce(Vector3.up);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+            if (!thrustSound.isPlaying)
+            {
+                thrustSound.Play();
+            }
         }
-        if(Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.forward);
-        }
-        else if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(-Vector3.forward);
+        else
+        {
+            thrustSound.Stop();
         }
     }
 }
